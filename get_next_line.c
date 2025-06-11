171ee4f3f504c40016d89264bb7ed8a00a1f87af
wschafer <wschafer@student.42berlin.de>
2025-06-11 13:48:35 +0200

    Implement of main for tests

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t	BUFFER_SIZE = 1024;
+
 char	*get_next_line(int fd)
 {
-	
+	char			*line;
+	char			*buffer;
+	static	char	*stored;
+
+	if (fd == -1)
+		return (NULL);
+	stored = malloc(BUFFER_SIZE + 1);
+	read (fd, stored, BUFFER_SIZE);
+	line = stored;
+
+	return (line);
+}
+//int read(int fileDescriptor, void *buffer, size_t bytesToRead)
+
+int	main()
+{
+	char	*str;
+	int		fd;
+
+
+	fd = open("text.txt", O_RDONLY);
+	while (1)
+	{
+		str = get_next_line(fd);
+		if (str)
+			printf("%s", str);
+		else
+			break;
+	}
+	close(fd);
+	return (0);
 }
