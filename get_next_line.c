81e2a8b9a3fa15e2f9c4c7fccd417def9684fe91
wschafer <wschafer@student.42berlin.de>
2025-06-25 14:09:26 +0200

    Improwig get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -16,16 +16,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlcat(char *dest, const char *src, size_t dsize);
-
-
 size_t	BUFFER_SIZE = 1024;
 
 char	*get_next_line(int fd)
 {
-	int				len = 0;
+
 	char			*buffer;
 	static	char	*cache = NULL;
 	int				i;
@@ -33,29 +28,23 @@ char	*get_next_line(int fd)
 
 	if (fd == -1 || BUFFER_SIZE <= 0)
 		return (NULL);
-	i = 1;
+
 	while (1)
 	{
-		len = ft_strchr(cache, BUFFER_SIZE);
-		if (len)
+		if (ft_strchr(cache, '\n'))
 		{
-			str = ft_substr(buffer, len);
-			cache = ft_strcat(cache, buffer, len);
+			str = ft_substr(cache, 0, len);
+			cache = strdup(cache);
 			return (str);
 		}
 		buffer = malloc(BUFFER_SIZE + 1);
-		if (read (fd, buffer, BUFFER_SIZE))
+		if (!buffer)
+			return (NULL);
+		if (read(fd, buffer, BUFFER_SIZE))
 		{
-			len = ft_strchr(cache, BUFFER_SIZE);
-			if (len)
-			{
-				str = ft_substr(buffer, len);
-				cache = ft_strcat(cache, buffer, len);
-				return (str);
-			}
-			ft_strcat(cache, buffer, len);
+			buffer[BUFFER_SIZE] = '\0';
+			cache = ft_strjoin(cache, buffer);
 		}
-		return (cache);
 	}
 	return (NULL);
 }
